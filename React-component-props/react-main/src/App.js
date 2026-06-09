ecc0d7565a6e7c7d3b906eda83da44ed42fe29b9
Veeramani-07 <veeramani.k2024ece@sece.ac.in>
2026-06-09 15:53:31 +0530

    Add files via upload

---
+++ React-component-props/react-main/src/App.js
@@ -0,0 +1,16 @@
+ function App() {
+  const a = 10;
+  const b = 5;
+
+  return (
+    <div>
+      <h1>Arithmetic Operations</h1>
+      <p>Addition: {a + b}</p>
+      <p>Subtraction: {a - b}</p>
+      <p>Multiplication: {a * b}</p>
+      <p>Division: {a / b}</p>
+    </div>
+  );
+}
+
+export default App;
